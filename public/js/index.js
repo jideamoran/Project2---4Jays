e4465684eb03f3cbb1434a245a625b1da3ea6f16
jordan.werre <47542013+jwerre1@users.noreply.github.com>
2019-04-16 00:10:29 -0400

    backend merged with front end

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,32 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
+var $examplename = $("#example-name");
+var $examplelocation = $("#example-location");
 var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
 
-$(document).ready(function() {
+var $exampleSearch = $("#example-search");
+var $searchBtn = $("#search");
+var $exampleList = $("#example-list");
+var $saveList = $("#save-list");
+var $saveBtn = $("#save");
+
+var $parkList = $("#park-list");
+
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+
+  $.get("/api/user_data").then(function (data) {
+    loadSaved(data.id);
     $(".member-name").text(data.email);
+    $("#save").attr("userID", data.id);
+    $("#submitForm").attr("userName", data.email);
   });
 });
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -24,84 +36,324 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  saveFavorite: function (example) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/favorites",
+      data: JSON.stringify(example)
+    });
+  },
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  getSearch: function (zip) {
     return $.ajax({
-      url: "api/examples/" + id,
+      url: "api/examples/" + zip,
+      type: "GET"
+    });
+  },
+  getSavedLoad: function (input) {
+    return $.ajax({
+      url: "api/loads/" + input,
+      type: "GET"
+    });
+  },
+  getPark: function (input) {
+    return $.ajax({
+      url: "api/parking/" + input,
+      type: "GET"
+    });
+  },
+  deleteExample: function (id) {
+    return $.ajax({
+      url: "api/favorites/" + id,
       type: "DELETE"
     });
   }
 };
 
+var loadSaved = function (input) {
+  API.getSavedLoad(input).then(function (data) {
+    // console.log(data);
+    var $saves = data.Favorites.map(function (save) {
+      var $div = $("<div>")
+        .text(save.location)
+        .addClass("save-link")
+
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-location": save.location,
+          "data-id": save.id
+        })
+        .append($div);
+
+      var $button = $("<button>")
+        .addClass("success button float-left save-link")
+        .text("Search");
+
+      $li.append($button);
+
+      var $buttonDelete = $("<button>")
+        .addClass("alert button float-right delete")
+        .text("Delete");
+
+      $li.append($buttonDelete);
+
+      return $li;
+    });
+
+    $saveList.empty();
+
+    $saveList.append($saves);
+  });
+
+}
+
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
+      var $a = $("<div>")
+        .text(example.body);
+      // .attr("href", "/example/" + example.id);
+
+      var $pUser = $("<p>").text("User: " + example.user);
+      var $pZip = $("<p>").text("Zip Code: " + example.location);
 
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
           "data-id": example.id
         })
+        .append($pUser)
+        .append($pZip)
         .append($a);
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
 
-      $li.append($button);
+      // $li.append($button);
 
       return $li;
     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
+    $parkList.empty();
+    $parkList.append($examples);
   });
 };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
+  var userName = $(this)
+    .parent()
+    .attr("userName");
+
   var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+    user: userName,
+    location: $examplelocation.val().trim(),
+    body: $exampleDescription.val().trim()
   };
 
-  if (!(example.text && example.description)) {
+  if (!(example.location && example.body)) {
     alert("You must enter an example text and description!");
     return;
   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
+  API.saveExample(example).then(function () {
   });
-
-  $exampleText.val("");
+  
+  $examplename.val("");
+  $examplelocation.val("");
   $exampleDescription.val("");
 };
 
+var handleFormSearch = function (event) {
+  event.preventDefault();
+  var search = $exampleSearch.val().trim();
+
+  API.getSearch(search).then(function (data) {
+    var $examples = data.map(function (example) {
+      var $a = $("<td>")
+        .text(example.body)
+        .attr("data-label", "Description");
+      // .attr("href", "/example/" + example.id);
+
+      var $pUser = $("<td>").text(example.user).attr("data-label", "Username");
+      var $pZip = $("<td>").text(example.location).attr("data-label", "Description");
+
+      var $li = $("<tr>")
+        .attr({
+          "data-id": example.id
+        })
+        .append($pUser)
+        .append($pZip)
+        .append($a);
+
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
+
+      // $li.append($button);
+
+      return $li;
+    });
+    $exampleList.empty();
+    $parkList.empty();
+    $exampleList.append($examples);
+  });
+
+
+  API.getPark(search).then(function (data) {
+    console.log(data);
+    var $parkes = data.map(function (park) {
+      var $a = $("<td>")
+        .text(park.name)
+        .attr("data-label", "Name");
+
+      var $pUser = $("<td>").text(park.address + ", " + park.city + ", " + park.state + " " + park.zip).attr("data-label", "Address");;
+      var $pZip = $("<td>").text(park.price).attr("data-label", "Price");;
+      var $pPrice = $("<td>").text(park.start + " - " + park.end).attr("data-label", "Time");
+
+      var $li = $("<tr>")
+        .append($a)
+        .append($pUser)
+        .append($pZip)
+        .append($pPrice);
+   
+
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
+
+      // $li.append($button);
+
+      return $li;
+    });
+ 
+    $parkList.append($parkes);
+  });
+
+
+
+
+}
+
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+  API.deleteExample(idToDelete).then(function () {
+    $.get("/api/user_data").then(function (data) {
+      loadSaved(data.id);
+    });
   });
 };
 
+var saveFav = function () {
+  var loc = $exampleSearch.val().trim()
+  var object = {
+    UserId: $("#save").attr("userid"),
+    location: loc
+  }
+  API.saveFavorite(object).then(function () {
+  });
+  $exampleSearch.val("");
+  loadSaved(object.UserId);
+}
+
+var handleFavoriteClick = function () {
+  var locationToRun = $(this)
+    .parent()
+    .attr("data-location");
+
+  API.getSearch(locationToRun).then(function (data) {
+    var $examples = data.map(function (example) {
+        var $a = $("<td>")
+        .text(example.body)
+        .attr("data-label", "Description");
+      // .attr("href", "/example/" + example.id);
+
+      var $pUser = $("<td>").text(example.user).attr("data-label", "Username");
+      var $pZip = $("<td>").text(example.location).attr("data-label", "Description");
+
+      var $li = $("<tr>")
+        .attr({
+          "data-id": example.id
+        })
+        .append($pUser)
+        .append($pZip)
+        .append($a);
+
+
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
+
+      // $li.append($button);
+
+      return $li;
+    });
+    $exampleList.empty();
+    $parkList.empty();
+    $exampleList.append($examples);
+  });
+
+  API.getPark(locationToRun).then(function (data) {
+    console.log(data);
+    var $parkes = data.map(function (park) {
+      var $a = $("<td>")
+        .text(park.name)
+        .attr("data-label", "Name");
+
+      var $pUser = $("<td>").text(park.address + ", " + park.city + ", " + park.state + " " + park.zip).attr("data-label", "Address");;
+      var $pZip = $("<td>").text(park.price).attr("data-label", "Price");;
+      var $pPrice = $("<td>").text(park.start + " - " + park.end).attr("data-label", "Time");
+
+      var $li = $("<tr>")
+        .append($a)
+        .append($pUser)
+        .append($pZip)
+        .append($pPrice);
+
+      // var $button = $("<button>")
+      //   .addClass("btn btn-danger float-right delete")
+      //   .text("ｘ");
+
+      // $li.append($button);
+
+      return $li;
+    });
+ 
+    $parkList.append($parkes);
+  });
+};
+
+$("#logout").on("click", function() {
+  console.log("logout clicked")
+  return $.ajax({
+    url: "/logout",
+    type: "GET"
+  });
+})
+
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$searchBtn.on("click", handleFormSearch);
+$saveBtn.on("click", saveFav);
+$saveList.on("click", ".delete", handleDeleteBtnClick);
+
+$saveList.on("click", ".save-link", handleFavoriteClick)
